bae65ff42fbfe50bb74311a1c8cb3d79db154ce8
Kevin Castejon <pitikev1@hotmail.com>
2020-04-13 19:07:49 +0200

    updated jimp dependency version

--- index.js
+++ index.js
@@ -3,37 +3,33 @@ const fs = require('fs');
 const path = require('path');
 
 class ImageSlicer {
-  static slice(folder='./', extensions=['.png','.jpg','.jpeg'], tileWidth=8, tileHeight=8, outputPath="./output/", transparentColor=null) {
-    return new Promise(function(resolve, reject) {
-      if(!fs.existsSync(outputPath))
-        fs.mkdirSync(outputPath);
-      let dir = fs.readdirSync(folder).filter((elt, i) => {
-        return(extensions.includes(path.extname(folder+'/'+elt)))
-      });
-      let totalImagesToWrite=0;
-      let imagesWritten=0;
+  static slice(folder = './', extensions = ['.png', '.jpg', '.jpeg'], tileWidth = 8, tileHeight = 8, outputPath = './output/', transparentColor = null) {
+    return new Promise(((resolve, reject) => {
+      if (!fs.existsSync(outputPath)) fs.mkdirSync(outputPath);
+      const dir = fs.readdirSync(folder).filter((elt) => (extensions.includes(path.extname(`${folder}/${elt}`))));
+      let totalImagesToWrite = 0;
+      let imagesWritten = 0;
       dir.forEach((elt) => {
-        let ext = path.extname(folder+'/'+elt);
-        let base = path.basename(folder+'/'+elt, ext);
-        Jimp.read(folder+'/'+elt, (err, img) => {
+        const ext = path.extname(`${folder}/${elt}`);
+        const base = path.basename(`${folder}/${elt}`, ext);
+        Jimp.read(`${folder}/${elt}`, (err, img) => {
           if (err) reject(err);
-          let numTileWidth=Math.floor(img.bitmap.width/tileWidth);
-          let numTileHeight=Math.floor(img.bitmap.height/tileHeight);
-          let numTiles=numTileWidth*numTileHeight;
+          const numTileWidth = Math.floor(img.bitmap.width / tileWidth);
+          const numTileHeight = Math.floor(img.bitmap.height / tileHeight);
           let it = 0;
-          for (let i = 0; i < numTileHeight; i++) {
-            for (let j = 0; j < numTileWidth; j++) {
-              totalImagesToWrite++;
-              let imgClone=img.clone();
-              if(transparentColor){
+          for (let i = 0; i < numTileHeight; i += 1) {
+            for (let j = 0; j < numTileWidth; j += 1) {
+              totalImagesToWrite += 1;
+              const imgClone = img.clone();
+              if (transparentColor) {
                 imgClone.scan(0, 0, imgClone.bitmap.width, imgClone.bitmap.height, (x, y, idx) => {
                   const thisColor = {
                     r: imgClone.bitmap.data[idx + 0],
                     g: imgClone.bitmap.data[idx + 1],
                     b: imgClone.bitmap.data[idx + 2],
-                    a: imgClone.bitmap.data[idx + 3]
+                    a: imgClone.bitmap.data[idx + 3],
                   };
-                  if(thisColor.r === transparentColor.r && thisColor.g === transparentColor.g && thisColor.b === transparentColor.b && thisColor.a === transparentColor.a){
+                  if (thisColor.r === transparentColor.r && thisColor.g === transparentColor.g && thisColor.b === transparentColor.b && thisColor.a === transparentColor.a) {
                     imgClone.bitmap.data[idx + 0] = 0;
                     imgClone.bitmap.data[idx + 1] = 0;
                     imgClone.bitmap.data[idx + 2] = 0;
@@ -41,24 +37,24 @@ class ImageSlicer {
                   }
                 });
               }
-              if(numTileHeight===1 && numTileWidth===1){
-                imgClone.write(path.resolve(outputPath,base+ext),onImageWritten);
+              if (numTileHeight === 1 && numTileWidth === 1) {
+                imgClone.write(path.resolve(outputPath, base + ext), onImageWritten);
               } else {
-                imgClone.crop( j*tileWidth, i*tileHeight, tileWidth, tileHeight );
-                imgClone.write(path.resolve(outputPath,base+'_'+it+ext),onImageWritten);
+                imgClone.crop(j * tileWidth, i * tileHeight, tileWidth, tileHeight);
+                imgClone.write(path.resolve(outputPath, `${base}_${it}${ext}`), onImageWritten);
               }
-              it++;
+              it += 1;
             }
           }
         });
       });
       function onImageWritten() {
-        imagesWritten++;
-        if(imagesWritten===totalImagesToWrite){
+        imagesWritten += 1;
+        if (imagesWritten === totalImagesToWrite) {
           resolve(imagesWritten);
         }
       }
-    });
+    }));
   }
 }
 module.exports = ImageSlicer;
--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 const ImageSlicer = require('../index');
-ImageSlicer.slice('./test/imagesFolder', ['.png','.jpg'], 16, 16, './test/output', {r:255, g:255, b:255, a:255})
-.then((numberImagesWritten) => {
-  console.log(numberImagesWritten.toString(),'images written');
-}).catch((err) => {
-  console.log(err);
-});
+
+ImageSlicer.slice('./test/imagesFolder', ['.png', '.jpg'], 16, 16, './test/output', {
+  r: 255, g: 255, b: 255, a: 255,
+})
+  .then((numberImagesWritten) => {
+    console.log(numberImagesWritten.toString(), 'images written');
+  }).catch((err) => {
+    console.log(err);
+  });
